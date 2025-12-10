adf97a54410af3e78138836b495b75ef9175d570
Prabal Kaushik <82644920+prabalkaushik@users.noreply.github.com>
2025-12-10 22:56:59 +0530

    initial project commit

---
+++ src/main/java/java/product/renting/ProductRentingAppApplication.java
@@ -0,0 +1,13 @@
+package java.product.renting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRentingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRentingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/product/renting/ProductRentingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java.product.renting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRentingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
